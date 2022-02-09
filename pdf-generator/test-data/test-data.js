f86e7ed99052a2156b899ae25b2f43b3cbc4baee
vineet <vineet@digitizeco.in>
2022-02-09 15:24:38 +0530

    updated logic for pdf generation

--- pdf-generator/test-data/test-data.js
+++ pdf-generator/test-data/test-data.js
@@ -2,7 +2,26 @@ const testData = {
     "sampleImage": "https://raw.githubusercontent.com/theVineetTanwar/theVineetTanwar/main/oakmasters-icon.jpg",
     "oakmasterLogo":'https://raw.githubusercontent.com/theVineetTanwar/theVineetTanwar/main/oakmaster-logo.png',
     data: {
-      netPrice: 1000
+      netPrice: 1000,
+      vatPrice: 200,
+      grossPrice: 1200,
+      "Door 2": true,
+      "Door 3": true,
+      "Right Log Store": true,
+      "Door 4": true,
+      "Right Roof Type": "Barn-Hip",
+      "View": "Outside",
+      "Wall Cladding Material": "Oak",
+      "Number Of Bays": "4",
+      "Left Log Store": true,
+      "Left Roof Type": "Full-Hip",
+      "Roof Pitch": "35-Deg",
+      "Tour": "Top",
+      "Door Material": "Softwood",
+      "Partition 1": true,
+      "Partition 2": true,
+      "Partition 3": true,
+      "Door 1": true
     }
     
   }
